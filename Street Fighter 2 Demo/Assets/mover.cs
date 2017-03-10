230b6e6e1b952ee166ef92578aa4f916f85f3330
Heng <hcheang1@my.centennialcollege.ca>
2017-03-10 13:28:01 -0500

    Moving Fireball

---
+++ Street Fighter 2 Demo/Assets/mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = transform.right * speed;
+		
+	}
+}
